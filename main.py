a37115a4ab382eed600e4d65a68a32cd71fb3670
rnjacky777 <rnjacky777@gmail.com>
2025-02-24 01:53:52 +0800

    Fix : CORSMiddleware issue

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from routers import login
 from util.auth import verify_password, get_password_hash, create_access_token
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(title="Modular FastAPI Project")
 
-# 🔑 CORS 設定 (允許 React 的開發伺服器訪問 API)
+origins = [
+    "http://localhost",  # Vite React 開發環境
+    "http://127.0.0.1",  # 有時瀏覽器會用 127.0.0.1
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173"],  # React 執行位址
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"],             # 可允許的來源
+    allow_credentials=True,           # 是否允許攜帶 cookie
+    allow_methods=["*"],              # 允許所有 HTTP 方法 (GET, POST, PUT, DELETE...)
+    allow_headers=["*"],              # 允許所有 headers
 )
-
-app = FastAPI(title="Modular FastAPI Project")
 
 # 將不同路由模組註冊到主應用
 app.include_router(login.router, prefix="/auth", tags=["Users"])
 
-@app.get("/api/hello")
-async def read_hello():
-    return {"message": "Hello from FastAPI!"}
+# @app.get("/api/hello")
+# async def read_hello():
+#     return {"message": "Hello from FastAPI!"}
